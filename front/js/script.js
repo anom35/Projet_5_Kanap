b61c597f8246ed96d5bb7c9a13e910075f7bff04
anom35 <freelance35830@gmail.com>
2022-12-08 13:54:36 +0100

    Affichage de la liste des articles

---
+++ front/js/script.js
@@ -0,0 +1,89 @@
+// interroge la base de données
+fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => ajouteArticles(data))
+
+
+// fonction qui boucle pour afficher tous les articles
+function ajouteArticles(varData) {
+
+    varData.forEach((element) => {
+
+        // création des balises <a> et <article>
+        const artLink           = createLink(element._id)
+        const article = document.createElement("article")
+
+        // création des balises <img>, <h3> et <p>
+        console.log(element)
+        // const _varUrlImg           = element.imageUrl
+        // const _varAltTxt           = element.altTxt
+        // const _varName             = element.name
+        // const _varDescription      = element.description
+
+        const artImage          = createImg(element.imageUrl, element.altTxt)
+        const artH3             = createH3(element.name)
+        const artDescription    = CreateParagraphe(element.description)
+
+        loadElementsArticle(article, artImage, artH3, artDescription)
+        AjoutArticle(artLink, article)
+    })
+}
+
+// function de chargement des éléments de la balise <article>
+function loadElementsArticle(baliseArticle, artImage, artH3, artDescription) {
+    baliseArticle.appendChild(artImage)
+    baliseArticle.appendChild(artH3)
+    baliseArticle.appendChild(artDescription)
+}
+
+// fonction de création de code dans la section avec l'ID #items
+function AjoutArticle(varId, article) {
+    const items = document.querySelector("#items")
+
+    // s'il trouve l'ID #items alors il exécute la condition
+    if (items != null) {
+      items.appendChild(varId)
+      varId.appendChild(article)
+    }
+}
+
+// fonction de création de la balise de lien <a>
+function createLink(varId) {
+    const baliseLink = document.createElement("a")
+    baliseLink.href = ".product.html?id=" + varId
+    return baliseLink
+}
+
+// fonction de création de la balise image <img>
+function createImg(varImageUrl, varAltTxt) {
+    const image = document.createElement("img")
+    image.src = varImageUrl
+    image.alt = varAltTxt
+
+    // supprime les attributs non utilisés
+    image.removeAttribute("title")
+    image.removeAttribute("style")
+    return image
+  }
+  
+
+// fonction de création de la balise <H3> + la classe productName <h3 class="productName">
+function createH3(name) {
+    const varH3 = document.createElement("h3")
+    varH3.textContent = name
+
+    // création de la classe productName
+    varH3.classList.add("productName")
+    return varH3
+}
+
+
+// fonction de création de la balise <p> + la classe productDescription <p class="productDescription">
+function CreateParagraphe(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+
+    // création de la classe productDescription
+    p.classList.add("productDescription")
+    return p
+}
