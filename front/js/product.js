6fcd09a1dca04e407aef8896ff437db1d0e354cf
anom35 <freelance35830@gmail.com>
2022-12-09 05:45:49 +0100

    Ajout de 2 nouvelles fonctions de validation

--- front/js/product.js
+++ front/js/product.js
@@ -12,41 +12,36 @@ fetch(`http://localhost:3000/api/products/${varId}`)
 
 // fonction appelé directement
 function loadCard(data) {
+
     console.log(data)
     createImg(data.imageUrl, data.altTxt)           // appelle des fonctions
     createPrice(data.price)                         // 
     createDescription(data.description)             //
     createTitle(data.name)                          //
 
-    for (cpt = 0; cpt <= 2; cpt++) {
-        createChoice(data.colors[cpt])              // boucle pour appeler la fonction et charger les 3 couleurs
+    for (cpt = 0; cpt < data.colors.length; cpt++) {
+        createChoice(data.colors[cpt])              // boucle pour charger les 3 couleurs
     }
 }
 
 // function de mise à jour du prix
 function createPrice(varPrice) {
     const parent = document.querySelector("#price") // cherche l'ID price
-    if (parent != null) {                           // si ID existant, alors je continue
-        parent.textContent = varPrice               // ajoute le prix dans la balise <h1>
-    }
+    valideAppendData(parent, varPrice)              // ajoute le prix dans la balise <span>
 }
 
 
 // function de mise à jour de la description
 function createDescription(varDescription) {
     const parent = document.querySelector("#description") // cherche l'ID description
-    if (parent != null) {                                 // si ID existant, alors je continue
-        parent.textContent = varDescription               // ajoute le prix dans la balise <p>
-    }
+    valideAppendData(parent, varDescription)              // ajoute la description dans la balise <p>
 }
 
 
 // function de mise à jour du titre
 function createTitle(varTitle) {
     const parent = document.querySelector("#title") // cherche l'ID title
-    if (parent != null) {                           // si ID existant, alors je continue
-        parent.textContent = varTitle               // ajoute le prix dans la balise <h1>
-    }
+    valideAppendData(parent, varTitle)              // ajoute le prix dans la balise <h1>
 }
 
 
@@ -55,14 +50,8 @@ function createImg(varImageUrl, varAltTxt) {
     const image = document.createElement("img")     // créer l'élément image
     image.src = varImageUrl                         // affecte l'URL source
     image.alt = varAltTxt                           // affecte le texte ALT
-
-    // supprime les attributs non utilisés qui apparaissent par défaut
-    image.removeAttribute("title")
-    image.removeAttribute("style")
-    const parent = document.querySelector(".item__img")
-    if (parent != null) {                            // test si le parent recherché existe bien
-        parent.appendChild(image)                    // ajoute la balise <img>
-    }
+    const parent = document.querySelector(".item__img")  // sélectionne la class="item__img"
+    valideAppendChild(parent, image)                // ajoute la balise <img>
 }
 
 
@@ -72,7 +61,19 @@ function createChoice(varChoice) {
     varOption.value = varChoice                        // affecte la valeur de value
     varOption.textContent = varChoice                  // affecte le texte affiché
     const parent = document.querySelector("#colors")
-    if (parent != null) {                              // test si le parent recherché existe bien
-        parent.appendChild(varOption)                  // ajoute la balise <option>
+    valideAppendChild(parent, varOption)                  // ajoute la balise <option>
+}
+
+// fonction qui test et ajoute une balise enfant
+function valideAppendChild(varParent, varValue) {
+    if (varParent != null) {                              // test si le parent recherché existe bien
+        varParent.appendChild(varValue)                   // ajoute la balise
+    }
+}
+
+// fonction qui test et ajoute des données à une balise
+function valideAppendData(varParent, varValue) {
+    if (varParent != null) {                              // test si le parent recherché existe bien
+        varParent.textContent = varValue                  // ajoute les données
     }
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,88 +1,78 @@
 // interroge la base de données
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => ajouteArticles(data))
+    .then((data) => ajouteArticles(data))               // resultat envoyé à la function ajouteArticles
 
 
 // fonction qui boucle pour afficher tous les articles
 function ajouteArticles(varData) {
+    
+    for (let cpt = 0; cpt < varData.length; cpt++) {    // boucle sur tous les articles
+        element = varData[cpt]                          // affecte l'élément en cours à la variable element
 
-    for (let cpt = 0; cpt < varData.length; cpt++) {
-        element = varData[cpt]
+        const artLink = createLink(element._id)             // création des balises <a> + lien
+        const article = document.createElement("article")   // création des balises <article>
 
-        // création des balises <a> et <article>
-        const artLink = createLink(element._id)
-        const article = document.createElement("article")
-
-        // A TESTER
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-        // ex: const { a, b } = obj;   // affecte aux variables a et b les valeurs de l'objet obj
+        //! A TESTER
+        //? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+        //? ex: const { a, b } = obj;   // affecte aux variables a et b les valeurs de l'objet obj
 
         // création des balises <img>, <h3> et <p>
         const artImage          = createImg(element.imageUrl, element.altTxt)
         const artH3             = createH3(element.name)
         const artDescription    = CreateParagraphe(element.description)
 
-        loadElementsArticle(article, artImage, artH3, artDescription)
-        AjoutArticle(artLink, article)
+        loadElementsArticle(article, artImage, artH3, artDescription)   // ajoute des balises <img>, <h3>, <p> dans la balise <article>
+        AjoutArticle(artLink, article)              // ajoute le lien de l'article en cours et la balise <article>
+        console.log(artLink + " - " + article)
     }
 }
 
 // function de chargement des éléments de la balise <article>
 function loadElementsArticle(baliseArticle, artImage, artH3, artDescription) {
-    baliseArticle.appendChild(artImage)
-    baliseArticle.appendChild(artH3)
-    baliseArticle.appendChild(artDescription)
+    baliseArticle.appendChild(artImage)             // ajoute la balise <img> dans la balise <article>
+    baliseArticle.appendChild(artH3)                // ajoute la balise <h3> dans la balise <article>
+    baliseArticle.appendChild(artDescription)       // ajoute la balise <p> dans la balise <article>
 }
 
 // fonction de création de code dans la section avec l'ID #items
 function AjoutArticle(varId, article) {
-    const items = document.querySelector("#items")
-
-    // s'il trouve l'ID #items alors il exécute la condition
-    if (items != null) {
-      items.appendChild(varId)
-      varId.appendChild(article)
+    const items = document.querySelector("#items")  // cherhche l'ID #items et affecte le resultat à items
+    if (items != null) {                            // s'il trouve l'ID #items, il exécute la condition
+      items.appendChild(varId)                      // ajoute la balise <a> avec le lien du produit
+      varId.appendChild(article)                    // ajoute la balise <article>
     }
 }
 
 // fonction de création de la balise de lien <a>
 function createLink(varId) {
-    const baliseLink = document.createElement("a")
-    baliseLink.href = "product.html?id=" + varId
+    const baliseLink = document.createElement("a")  // création de la balise <a>
+    baliseLink.href = "product.html?id=" + varId    // ajoute à l'attribut href="" le chemin de l'article
     return baliseLink
 }
 
 // fonction de création de la balise image <img>
 function createImg(varImageUrl, varAltTxt) {
-    const image = document.createElement("img")
-    image.src = varImageUrl
-    image.alt = varAltTxt
-
-    // supprime les attributs non utilisés qui apparaissent par défaut
-    image.removeAttribute("title")
-    image.removeAttribute("style")
+    const image = document.createElement("img")     // création de la balise <img>
+    image.src = varImageUrl                         // ajoute l'URL à l'attribut src=""
+    image.alt = varAltTxt                           // ajoute le texte de l'attribut alt=""
     return image
-  }
+}
   
 
 // fonction de création de la balise <H3> + la classe productName <h3 class="productName">
 function createH3(name) {
-    const varH3 = document.createElement("h3")
-    varH3.textContent = name
-
-    // création de la classe productName
-    varH3.classList.add("productName")
+    const varH3 = document.createElement("h3")      // création de la balise <h3>
+    varH3.textContent = name                        // ajoute le texte de la balise H3
+    varH3.classList.add("productName")              // création de la classe productName
     return varH3
 }
 
 
 // fonction de création de la balise <p> + la classe productDescription <p class="productDescription">
 function CreateParagraphe(description) {
-    const p = document.createElement("p")
-    p.textContent = description
-
-    // création de la classe productDescription
-    p.classList.add("productDescription")
+    const p = document.createElement("p")           // creation de la balise <p>
+    p.textContent = description                     // ajout le texte de la description
+    p.classList.add("productDescription")           // création de la classe productDescription
     return p
 }
