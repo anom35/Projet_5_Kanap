9eb80f77d2320116f47784319ed9aba9c5f60cbe
anom35 <freelance35830@gmail.com>
2022-12-26 18:06:11 +0100

    Modification des fonctions

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,19 +1,29 @@
-let arrayItems  = []
-let valeurTotal = 0
 if (localStorage.length > 0) {
-    for (let cpt in localStorage) {
+    let arrayItems      = []
+    let arrayStorage    = []
+    let valeurTotal     = 0
+    let totalArticles   = 0
+    for (let cpt=0; cpt < localStorage.length; cpt++) {
         console.log(cpt)
-        arrayItems.push(JSON.parse(localStorage.getItem(localStorage.key(cpt))))
+        arrayStorage = JSON.parse(localStorage.getItem(localStorage.key(cpt)))
+        arrayItems = retrieveProduct(arrayStorage.id)
 
-        valeurTotal += arrayItems[cpt].price * arrayItems[cpt].quantity
-        totalArticles += arrayItems[cpt].quantity
-
+        console.log("varId:",arrayStorage.id)
+        valeurTotal += arrayItems.price * arrayItems.quantity
+        totalArticles += arrayItems.quantity
+        console.log(arrayItems)
         let parent = document.querySelector("#cart__items")
         if (parent != null) {
-            createAndAffectStruct(parent, cpt)
+            createAndAffectStruct(parent, cpt, arrayItems)
         }
     }
     displayTotal(valeurTotal, totalArticles)
+}
+
+function retrieveProduct(varId) {
+    fetch("http://localhost:3000/api/products/" + varId)  
+    .then((res) => res.json())
+    .then((data) => { return data })
 }
 
 // création d'évennements pour les boutons "supprimer"
@@ -25,10 +35,8 @@ varClickDelete.forEach(function(elem) {
 // création d'évennements pour les boutons "supprimer"
 let varClickModifyQuantity = document.querySelectorAll(".itemQuantity")   
 varClickModifyQuantity.forEach(function(elem) {
-    elem.addEventListener("click", modifyQuantity);              
+    elem.addEventListener("click", changeQuantityFromCart(elem.id))
 })
-
-
 
 // function qui affiche le nombre total d'article et le total
 function displayTotal(valeur, quantity) {
@@ -42,22 +50,23 @@ function displayTotal(valeur, quantity) {
 }
 
 // fonction qui créer la structure HTML et affecte les valeurs du panier
-function createAndAffectStruct(parent, cpt) {
+function createAndAffectStruct(parent, cpt, arrayItems) {
+    console.log(arrayItems)
     parent.innerHTML += `
-    <article class="cart__item" data-id="${arrayItems[cpt].id}" data-color="${arrayItems[cpt].color}">
+    <article class="cart__item" data-id="${arrayItems.id}" data-color="${arrayItems.color}">
         <div class="cart__item__img">
-          <img src="${arrayItems[cpt].image}" alt="Photographie d'un canapé">
+          <img src="${arrayItems.image}" alt="Photographie d'un canapé">
         </div>
         <div class="cart__item__content">
             <div class="cart__item__content__description">
-                <h2>${arrayItems[cpt].title}</h2>
-                <p>Couleur : ${arrayItems[cpt].color}</p>
-                <p>Prix : ${arrayItems[cpt].price}</p>
+                <h2>${arrayItems.title}</h2>
+                <p>Couleur : ${arrayItems.color}</p>
+                <p>Prix : ${arrayItems.price}</p>
             </div>
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
-                    <p>Qté : ${arrayItems[cpt].quantity}</p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${arrayItems[cpt].quantity}">
+                    <p>Qté : ${arrayItems.quantity}</p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${arrayItems.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                 <p class="deleteItem">Supprimer</p>
@@ -68,3 +77,60 @@ function createAndAffectStruct(parent, cpt) {
 }
 
 // location.reload()
+
+
+// fonction sauvegarde du panier dans le localstorage
+function saveCart(cart) {
+    localStorage.setItem(cart.id, JSON.stringify(cart))
+}
+
+// fonction récupère un article
+function getCart(varId) {
+    let panier = JSON.parse(localStorage.getItem(varId))
+    if (panier == null ) {
+        return []
+    } else {
+        return JSON.parse(panier)
+    }
+}
+
+// fonction qui supprimer un article du panier
+function deleteArticleFromCart(product) {
+    let panier = getCart(product)
+    panier = panier.filter(p => p.id != product.id) // filtre tous ceux qui sont différent et supprime celui qui est indentique
+    saveCart(panier)
+}
+
+// fonction qui change la quantité d'un article dans le panier
+function changeQuantityFromCart(product, quantity) {
+    let panier = getCart(product)
+    let productExist = panier.find(p => p.id == product.id)
+    if (productExist != undefined) {
+        productExist.quantity += quantity
+        if (productExist.quantity <= 0) {
+            deleteArticleFromCart(productExist)
+        } else {
+            saveCart(panier)
+        }
+    } 
+}
+
+// fonction qui calcul le nombre d'articles total dans le panier
+function calculNumberArticles() {
+    let panier = getCart()
+    let number = 0
+    for (let article of panier) {
+        number += article.quantity
+    }
+    return number
+}
+
+// fonction qui calcul le prix total dans le panier
+function calculTotalPrice() {
+    let panier = getCart()
+    let total = 0
+    for (let article of panier) {
+        total += article.price
+    }
+    return total
+}
--- front/js/product.js
+++ front/js/product.js
@@ -20,18 +20,13 @@ function searchProduct() {
 function loadCard(data) {
     console.log(data.id)
     let parent = document.querySelector(".item__img")
-    parent.innerHTML += "<img src=\""+ data.imageUrl+ "\" alt=\"" + data.altTxt + "\">"
-    console.log("<img src=\""+ data.imageUrl+ "\" alt=\"" + data.altTxt + "\">")
-
+    parent.innerHTML += "<img src=\""+ data.imageUrl + "\" alt=\"" + data.altTxt + "\">"
     parent = document.querySelector("#title")
     parent.textContent = data.name
-
     parent = document.querySelector("#price")
     parent.textContent = data.price
-
     parent = document.querySelector("#description")
     parent.textContent = data.description
-
     for (cpt = 0; cpt < data.colors.length; cpt++) {
         createChoice(data.colors[cpt])                
     }
@@ -44,7 +39,6 @@ function createChoice(varChoice) {
     const varOption = document.createElement("option")
     varOption.value = varChoice                       
     varOption.textContent = varChoice                 
-
     const parent = document.querySelector("#colors")
     parent.appendChild(varOption)    
 }
@@ -54,25 +48,20 @@ const varAddArticle = document.querySelector("#addToCart")
 varAddArticle.addEventListener("click", addQuantityToCart);  
 
 function addQuantityToCart() {
-
     const quantityInput = document.querySelector("#quantity")
     const varQuantity = quantityInput.value                  
     const choiceColor = document.querySelector("#colors")    
     const varColor = choiceColor.value                       
 
     if ((varQuantity > 0) && (varColor != "")) {
-
         colorGrisBorder()                                     // remet les bordures en gris
-        console.log("------  enregistrement  -------")
         let objJson = {    
             id      : varId,
             color   : varColor,
             quantity: parseInt(varQuantity)
          }
-
-        searchDuplicate(varId, varColor, varQuantity)
-        let objCart = JSON.stringify(objJson);                // transforme un objet en texte json
-        localStorage.setItem(varId, objCart);                 // sauvegarde dans le localstorage
+        // searchDuplicate(varId, varColor, varQuantity)
+        saveCart(objJson)
         document.location.href="index.html"
     }
     else {
@@ -81,35 +70,7 @@ function addQuantityToCart() {
 }
 
 
-//! A TESTER
-function searchDuplicate(id, color, quantity) {
-    let varColor    = ""
-    let arrayItems  = []
-    let varQuantity = quantity
-    let a, valKey   = 0
-
-    // compte si plusieurs articles identiques existe
-    for (let cpt=0; cpt < localStorage.length; cpt++) {
-        arrayItems.push(JSON.parse(localStorage.getItem(localStorage.key(cpt))))
-        if ((arrayItems[cpt].id === id) && (arrayItems[cpt].color === color)) {
-            a += 1
-        }
-    }
-    if (a >= 2) {
-        // si plusieurs acticles existent, il faut cumuler les quantités et n'en laisser qu'un
-        for (let cpt=0; cpt < localStorage.length; cpt++) {
-            if ((arrayItems[cpt].id === id) && (arrayItems[cpt].color === color)) {
-                varQuantity += arrayItems[cpt].quantity
-            }
-        }
-    }
-
-}
-
-
-
-
-// fonction qui test si les champs sont remplis
+// fonction qui test si les champs sont remplis, sinon change les bordures en rouge
 function testContentFields(varQuantity, varColor) {
     if (varQuantity <= 0) { 
         const varElement = document.querySelector("input")              // sinon la bordure passe en rouge
@@ -132,7 +93,7 @@ function testContentFields(varQuantity, varColor) {
 }
 
 
-
+// fonction qui met les bordures en gris
 function colorGrisBorder() {                                                // remet les bordures en gris
     const varInput = document.querySelector("input")
     const varQuantity = document.getElementById("#quantity")
@@ -141,4 +102,19 @@ function colorGrisBorder() {                                                // r
     const varSelect = document.querySelector("select")
     const varColors = document.getElementById("#colors")
     varSelect.setAttribute("style", "border:1px solid #767676;")
+}
+
+// fonction sauvegarde du panier dans le localstorage
+function saveCart(cart) {
+    localStorage.setItem(cart.id, JSON.stringify(cart))
+}
+
+// fonction récupère un article
+function getCart(varId) {
+    let panier = JSON.parse(localStorage.getItem(varId))
+    if (panier == null ) {
+        return []
+    } else {
+        return JASON.parse(panier)
+    }
 }
